2f7f9d43a717afe46a3156dd10d4fef1383118db
hadyy17 <19cshad166@ldce.ac.in>
2022-06-17 21:56:43 +0530

    Testing commit

---
+++ connect.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import asyncio
+
+import websockets
+
+
+#async def handler(websocket):
+	#while True:
+    	#    try:
+      # 	message = await websocket.recv()
+        #except websockets.ConnectionClosedOK:
+        #	break
+        #print(message)
+
+
+
+async def handler(websocket):
+    async for message in websocket:
+        print(message)
+
+
+async def main():
+    async with websockets.serve(handler, "", 8001):
+        await asyncio.Future()  # run forever
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
