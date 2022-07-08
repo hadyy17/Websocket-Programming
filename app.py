f13eb882e81ab32d3828d448aa86f36fc44d3133
hadyy17 <19cshad166@ldce.ac.in>
2022-07-08 10:13:52 +0530

    Commit

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python
 
-import asyncio
-import json
-import secrets
+import signal
 
 import websockets
 
@@ -182,8 +179,14 @@ async def handler(websocket):
 
 
 async def main():
-    async with websockets.serve(handler, "", 8001):
-        await asyncio.Future()  # run forever
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    port = int(os.environ.get("PORT", "8001"))
+    async with websockets.serve(handler, "", port):
+        await stop
 
 
 if __name__ == "__main__":
