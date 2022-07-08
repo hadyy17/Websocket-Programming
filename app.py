f12c97ca5ff4d08d5d995ae2b864c7692ec2d018
hadyy17 <19cshad166@ldce.ac.in>
2022-07-08 10:03:22 +0530

    Commit

--- app.py
+++ app.py
@@ -1,67 +1,190 @@
-import websocket
-
-1#!/usr/bin/env python
- 2
- 3import asyncio
- 4import itertools
- 5import json
- 6
- 7import websockets
- 8
- 9from connect import PLAYER1, PLAYER2, Connect4
-10
-11
-12async def handler(websocket):
-13    # Initialize a Connect Four game.
-14    game = Connect4()
-15
-16    # Players take alternate turns, using the same browser.
-17    turns = itertools.cycle([PLAYER1, PLAYER2])
-18    player = next(turns)
-19
-20    async for message in websocket:
-21        # Parse a "play" event from the UI.
-22        event = json.loads(message)
-23        assert event["type"] == "play"
-24        column = event["column"]
-25
-26        try:
-27            # Play the move.
-28            row = game.play(player, column)
-29        except RuntimeError as exc:
-30            # Send an "error" event if the move was illegal.
-31            event = {
-32                "type": "error",
-33                "message": str(exc),
-34            }
-35            await websocket.send(json.dumps(event))
-36            continue
-37
-38        # Send a "play" event to update the UI.
-39        event = {
-40            "type": "play",
-41            "player": player,
-42            "column": column,
-43            "row": row,
-44        }
-45        await websocket.send(json.dumps(event))
-46
-47        # If move is winning, send a "win" event.
-48        if game.winner is not None:
-49            event = {
-50                "type": "win",
-51                "player": game.winner,
-52            }
-53            await websocket.send(json.dumps(event))
-54
-55        # Alternate turns.
-56        player = next(turns)
-57
-58
-59async def main():
-60    async with websockets.serve(handler, "", 8001):
-61        await asyncio.Future()  # run forever
-62
-63
-64if __name__ == "__main__":
-65    asyncio.run(main())
+  1#!/usr/bin/env python
+  2
+  3import asyncio
+  4import json
+  5import secrets
+  6
+  7import websockets
+  8
+  9from connect4 import PLAYER1, PLAYER2, Connect4
+ 10
+ 11
+ 12JOIN = {}
+ 13
+ 14WATCH = {}
+ 15
+ 16
+ 17async def error(websocket, message):
+ 18    """
+ 19    Send an error message.
+ 20
+ 21    """
+ 22    event = {
+ 23        "type": "error",
+ 24        "message": message,
+ 25    }
+ 26    await websocket.send(json.dumps(event))
+ 27
+ 28
+ 29async def replay(websocket, game):
+ 30    """
+ 31    Send previous moves.
+ 32
+ 33    """
+ 34    # Make a copy to avoid an exception if game.moves changes while iteration
+ 35    # is in progress. If a move is played while replay is running, moves will
+ 36    # be sent out of order but each move will be sent once and eventually the
+ 37    # UI will be consistent.
+ 38    for player, column, row in game.moves.copy():
+ 39        event = {
+ 40            "type": "play",
+ 41            "player": player,
+ 42            "column": column,
+ 43            "row": row,
+ 44        }
+ 45        await websocket.send(json.dumps(event))
+ 46
+ 47
+ 48async def play(websocket, game, player, connected):
+ 49    """
+ 50    Receive and process moves from a player.
+ 51
+ 52    """
+ 53    async for message in websocket:
+ 54        # Parse a "play" event from the UI.
+ 55        event = json.loads(message)
+ 56        assert event["type"] == "play"
+ 57        column = event["column"]
+ 58
+ 59        try:
+ 60            # Play the move.
+ 61            row = game.play(player, column)
+ 62        except RuntimeError as exc:
+ 63            # Send an "error" event if the move was illegal.
+ 64            await error(websocket, str(exc))
+ 65            continue
+ 66
+ 67        # Send a "play" event to update the UI.
+ 68        event = {
+ 69            "type": "play",
+ 70            "player": player,
+ 71            "column": column,
+ 72            "row": row,
+ 73        }
+ 74        websockets.broadcast(connected, json.dumps(event))
+ 75
+ 76        # If move is winning, send a "win" event.
+ 77        if game.winner is not None:
+ 78            event = {
+ 79                "type": "win",
+ 80                "player": game.winner,
+ 81            }
+ 82            websockets.broadcast(connected, json.dumps(event))
+ 83
+ 84
+ 85async def start(websocket):
+ 86    """
+ 87    Handle a connection from the first player: start a new game.
+ 88
+ 89    """
+ 90    # Initialize a Connect Four game, the set of WebSocket connections
+ 91    # receiving moves from this game, and secret access tokens.
+ 92    game = Connect4()
+ 93    connected = {websocket}
+ 94
+ 95    join_key = secrets.token_urlsafe(12)
+ 96    JOIN[join_key] = game, connected
+ 97
+ 98    watch_key = secrets.token_urlsafe(12)
+ 99    WATCH[watch_key] = game, connected
+100
+101    try:
+102        # Send the secret access tokens to the browser of the first player,
+103        # where they'll be used for building "join" and "watch" links.
+104        event = {
+105            "type": "init",
+106            "join": join_key,
+107            "watch": watch_key,
+108        }
+109        await websocket.send(json.dumps(event))
+110        # Receive and process moves from the first player.
+111        await play(websocket, game, PLAYER1, connected)
+112    finally:
+113        del JOIN[join_key]
+114        del WATCH[watch_key]
+115
+116
+117async def join(websocket, join_key):
+118    """
+119    Handle a connection from the second player: join an existing game.
+120
+121    """
+122    # Find the Connect Four game.
+123    try:
+124        game, connected = JOIN[join_key]
+125    except KeyError:
+126        await error(websocket, "Game not found.")
+127        return
+128
+129    # Register to receive moves from this game.
+130    connected.add(websocket)
+131    try:
+132        # Send the first move, in case the first player already played it.
+133        await replay(websocket, game)
+134        # Receive and process moves from the second player.
+135        await play(websocket, game, PLAYER2, connected)
+136    finally:
+137        connected.remove(websocket)
+138
+139
+140async def watch(websocket, watch_key):
+141    """
+142    Handle a connection from a spectator: watch an existing game.
+143
+144    """
+145    # Find the Connect Four game.
+146    try:
+147        game, connected = WATCH[watch_key]
+148    except KeyError:
+149        await error(websocket, "Game not found.")
+150        return
+151
+152    # Register to receive moves from this game.
+153    connected.add(websocket)
+154    try:
+155        # Send previous moves, in case the game already started.
+156        await replay(websocket, game)
+157        # Keep the connection open, but don't receive any messages.
+158        await websocket.wait_closed()
+159    finally:
+160        connected.remove(websocket)
+161
+162
+163async def handler(websocket):
+164    """
+165    Handle a connection and dispatch it according to who is connecting.
+166
+167    """
+168    # Receive and parse the "init" event from the UI.
+169    message = await websocket.recv()
+170    event = json.loads(message)
+171    assert event["type"] == "init"
+172
+173    if "join" in event:
+174        # Second player joins an existing game.
+175        await join(websocket, event["join"])
+176    elif "watch" in event:
+177        # Spectator watches an existing game.
+178        await watch(websocket, event["watch"])
+179    else:
+180        # First player starts a new game.
+181        await start(websocket)
+182
+183
+184async def main():
+185    async with websockets.serve(handler, "", 8001):
+186        await asyncio.Future()  # run forever
+187
+188
+189if __name__ == "__main__":
+190    asyncio.run(main())
--- main.js
+++ main.js
@@ -1,53 +1,83 @@
  1import { createBoard, playMove } from "./connect4.js";
  2
- 3function showMessage(message) {
- 4  window.setTimeout(() => window.alert(message), 50);
- 5}
- 6
- 7function receiveMoves(board, websocket) {
- 8  websocket.addEventListener("message", ({ data }) => {
- 9    const event = JSON.parse(data);
-10    switch (event.type) {
-11      case "play":
-12        // Update the UI with the move.
-13        playMove(board, event.player, event.column, event.row);
-14        break;
-15      case "win":
-16        showMessage(`Player ${event.player} wins!`);
-17        // No further messages are expected; close the WebSocket connection.
-18        websocket.close(1000);
-19        break;
-20      case "error":
-21        showMessage(event.message);
-22        break;
-23      default:
-24        throw new Error(`Unsupported event type: ${event.type}.`);
-25    }
-26  });
-27}
-28
-29function sendMoves(board, websocket) {
-30  // When clicking a column, send a "play" event for a move in that column.
-31  board.addEventListener("click", ({ target }) => {
-32    const column = target.dataset.column;
-33    // Ignore clicks outside a column.
-34    if (column === undefined) {
-35      return;
-36    }
-37    const event = {
-38      type: "play",
-39      column: parseInt(column, 10),
-40    };
-41    websocket.send(JSON.stringify(event));
-42  });
-43}
-44
-45window.addEventListener("DOMContentLoaded", () => {
-46  // Initialize the UI.
-47  const board = document.querySelector(".board");
-48  createBoard(board);
-49  // Open the WebSocket connection and register event handlers.
-50  const websocket = new WebSocket("ws://localhost:8001/");
-51  receiveMoves(board, websocket);
-52  sendMoves(board, websocket);
-53});
+ 3function initGame(websocket) {
+ 4  websocket.addEventListener("open", () => {
+ 5    // Send an "init" event according to who is connecting.
+ 6    const params = new URLSearchParams(window.location.search);
+ 7    let event = { type: "init" };
+ 8    if (params.has("join")) {
+ 9      // Second player joins an existing game.
+10      event.join = params.get("join");
+11    } else if (params.has("watch")) {
+12      // Spectator watches an existing game.
+13      event.watch = params.get("watch");
+14    } else {
+15      // First player starts a new game.
+16    }
+17    websocket.send(JSON.stringify(event));
+18  });
+19}
+20
+21function showMessage(message) {
+22  window.setTimeout(() => window.alert(message), 50);
+23}
+24
+25function receiveMoves(board, websocket) {
+26  websocket.addEventListener("message", ({ data }) => {
+27    const event = JSON.parse(data);
+28    switch (event.type) {
+29      case "init":
+30        // Create links for inviting the second player and spectators.
+31        document.querySelector(".join").href = "?join=" + event.join;
+32        document.querySelector(".watch").href = "?watch=" + event.watch;
+33        break;
+34      case "play":
+35        // Update the UI with the move.
+36        playMove(board, event.player, event.column, event.row);
+37        break;
+38      case "win":
+39        showMessage(`Player ${event.player} wins!`);
+40        // No further messages are expected; close the WebSocket connection.
+41        websocket.close(1000);
+42        break;
+43      case "error":
+44        showMessage(event.message);
+45        break;
+46      default:
+47        throw new Error(`Unsupported event type: ${event.type}.`);
+48    }
+49  });
+50}
+51
+52function sendMoves(board, websocket) {
+53  // Don't send moves for a spectator watching a game.
+54  const params = new URLSearchParams(window.location.search);
+55  if (params.has("watch")) {
+56    return;
+57  }
+58
+59  // When clicking a column, send a "play" event for a move in that column.
+60  board.addEventListener("click", ({ target }) => {
+61    const column = target.dataset.column;
+62    // Ignore clicks outside a column.
+63    if (column === undefined) {
+64      return;
+65    }
+66    const event = {
+67      type: "play",
+68      column: parseInt(column, 10),
+69    };
+70    websocket.send(JSON.stringify(event));
+71  });
+72}
+73
+74window.addEventListener("DOMContentLoaded", () => {
+75  // Initialize the UI.
+76  const board = document.querySelector(".board");
+77  createBoard(board);
+78  // Open the WebSocket connection and register event handlers.
+79  const websocket = new WebSocket("ws://localhost:8001/");
+80  initGame(websocket);
+81  receiveMoves(board, websocket);
+82  sendMoves(board, websocket);
+83});
